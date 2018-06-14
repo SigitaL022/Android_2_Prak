656f65ecacd00ec2ddc90c41a065999077a79bdb
sigit <s.lankaronska@inbox.lv>
2018-06-14 19:50:40 +0300

    Test

---
+++ app/src/main/java/com/example/sigita/otrais_praktiskai_darbs/RepositoryGetHub.java
@@ -0,0 +1,13 @@
+package com.example.sigita.otrais_praktiskai_darbs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RepositoryGetHub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository_get_hub);
+    }
+}
